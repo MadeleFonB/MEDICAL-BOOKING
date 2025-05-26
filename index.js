7903a7ced520e0faece3a307cd2f6224ec2f17bc
Madeleine Daniela Fonseca Bernal <128857984+MFONSE4@users.noreply.github.com>
2025-05-25 21:22:22 -0500

    Primer commit - Proyecto Medical Booking

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+require('dotenv').config(); 
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const typeDefs = require('./src/schema/typeDefs');
 const resolvers = require('./src/resolvers/resolvers');
 
 async function startServer() {
   const app = express();
 
-  // Conectar a MongoDB
   try {
-    await mongoose.connect('mongodb://localhost:27017/clinic', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -19,6 +19,7 @@ async function startServer() {
     console.error('❌ Error conectando a MongoDB:', err);
     process.exit(1);
   }
+
   app.use(express.static(path.join(__dirname, 'public')));
 
   const apolloServer = new ApolloServer({
@@ -32,7 +33,7 @@ async function startServer() {
 
   apolloServer.applyMiddleware({ app });
 
-  const PORT = 5050;
+  const PORT = process.env.PORT || 5050;
   app.listen(PORT, () => {
     console.log(`🚀 Servidor listo en http://localhost:${PORT}${apolloServer.graphqlPath}`);
     console.log(`🖥️  Frontend servido en http://localhost:${PORT}/index.html`);
